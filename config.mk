321386c7dbfe39fd394410f442757f091f232b5e
Peter Ogilvie <peter.ogilvie@coinbase.com>
2022-04-18 06:04:48 -0700

    update config.mk

--- config.mk
+++ config.mk
@@ -2,6 +2,6 @@
 # The varible dev is set to the cli authorized connection with the alias dev
 dev = demo
 connectedAppName = Demo
-grantUsername = code@curious-wolf-2cylyt.com
-clientId = 3MVG9uOrYDwSZBEbddb_Mr2Oimuy5HKi_Z68CI12.fxmvmPqzxJnn7gljB9IdEU6GQKaqH_ALqC9qlWSzL0MU
-scratchUsername = test-9w5hx6zxmzbd@example.com
+grantUsername = TBD
+clientId = TBD
+scratchUsername = TBD
